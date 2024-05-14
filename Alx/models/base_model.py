a6b5d01a08ac1dcbdd9897b902152a863d116cd0
Abu Jehoiada <abujehoiada@gmail.com>
2024-05-14 15:44:43 +0100

    Fixed code

---
+++ Alx/models/base_model.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+"""Module for the basemodel"""
+
+
+import uuid
+from datetime import datetime
+
+class BaseModel:
+    """Base model class"""
+    def __init__(self, *args, **kwargs):
+        """Initialization of the Base Model class"""
+        if kwargs:
+            for key, value in kwargs.items():
+                if key == '__class__':
+                    continue
+                elif key == 'created_at' or 'updated_at':
+                    self.__dict__[key] = datetime.fromisoformat(value)
+                else:
+                    self.__dict__[key] = value               
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
+
+    def __str__(self):
+        """String representation of the class"""
+        class_name = self.__class__.__name__
+        return f"[{class_name}] ({self.id}) {self.__dict__}"
+    
+    def save(self):
+        """Saves progress"""
+        self.updated_at = datetime.now()
+
+    def to_dict(self):
+        obj_dict = self.__dict__.copy()
+        obj_dict['__class__'] = self.__class__.__name__
+        self.created_at.isoformat()
+        self.updated_at.isoformat()
+        return obj_dict
+    
